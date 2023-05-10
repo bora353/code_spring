2c0014669a56fe34c5f61bcd20036670e0a80348
bora353 <117142490+bora353@users.noreply.github.com>
2023-05-10 16:20:50 +0900

    코드로 배우는 스프링 웹 프로젝트

---
+++ springstudy/src/main/java/com/mystudy/mapper/TimeMapper.java
@@ -0,0 +1,12 @@
+package com.mystudy.mapper;
+
+import org.apache.ibatis.annotations.Select;
+
+public interface TimeMapper {
+
+	@Select("SELECT sysdate from dual")
+	public String getTime();
+	
+	public String getTime2();
+	
+}
